0ff64636463ac8d6a65997d1f5b03d3456498e44
Marina <elanor.me@gmail.com>
2020-05-24 22:54:34 +0300

    Updates

---
+++ precache-manifest.3096af073f6fafce0629d0de91514413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d28c674670a20921a331fd5db51710",
+    "url": "/post-reader-react/index.html"
+  },
+  {
+    "revision": "1fc966e1346760bdc58b",
+    "url": "/post-reader-react/static/css/main.8acb6e54.chunk.css"
+  },
+  {
+    "revision": "e39285f63256b9906576",
+    "url": "/post-reader-react/static/js/2.11f99e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/post-reader-react/static/js/2.11f99e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc966e1346760bdc58b",
+    "url": "/post-reader-react/static/js/main.dcad46ee.chunk.js"
+  },
+  {
+    "revision": "23c44377768e34fb908e",
+    "url": "/post-reader-react/static/js/runtime-main.a29b41d3.js"
+  }
+]);
